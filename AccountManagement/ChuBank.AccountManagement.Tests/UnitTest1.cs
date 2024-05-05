93274c69a9c9b2176238edddcd504969fcdf6506
iBruns <igor.gomes.dev@gmail.com>
2024-05-05 16:27:20 -0300

    chore: create base domain driven folder structure for the AccountManagement bounded context

---
+++ AccountManagement/ChuBank.AccountManagement.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ChuBank.AccountManagement.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
